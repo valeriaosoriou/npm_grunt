e532219f18678acb8fc5ea66cb57e80061490945
Valeria <valeria.osoriou@gmail.com>
2020-02-18 14:44:00 +1300

    hola3

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    // uglify: {
+    //   options: {
+    //     banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    //   },
+    //   build: {
+    //     src: 'src/<%= pkg.name %>.js',
+    //     dest: 'build/<%= pkg.name %>.min.js'
+    //   }
+    // }
+    sass: {                              // Task
+    dist: {                            // Target
+      options: {                       // Target options
+        style: 'expanded'
+      },
+      files: {                         // Dictionary of files
+        'css/style.css': 'sass/style.scss',       // 'destination': 'source'
+        // 'widgets.css': 'widgets.scss'
+      }
+    }
+  },
+    jshint: {
+    all: ['Gruntfile.js', 'js/*.js']
+  }
+});
+  // Load the plugin that provides the "uglify" task.
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  // Default task(s).
+  // grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['sass','jshint']);
+};
---
+++ js/script.js
@@ -0,0 +1,6 @@
+console.log('NPM-GRUNT');
+$(document).ready(function(){
+  $('body').click(function(){
+    $(this).css('background','cyan');
+  });
+});
